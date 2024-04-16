3c07917b6859659e06e4c81a997d5d117f1d34a7
beracoechea <jose.beracoechea5098@alumnos.udg.mx>
2024-04-15 18:19:03 -0600

    Se logro el boton

--- JS/menu.js
+++ JS/menu.js
@@ -1,4 +1,16 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("show-menu");
-}
+(function(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+
+    });
+
+    closeMenu.addEventListener('click',()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+
+})();
